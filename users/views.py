673fba2df6ec66be6b51565f79fd7203c1eab60e
Michael Mai <micheal.chao@hotmail.com>
2023-01-14 19:01:01 +0800

    修改了用户修改密码API

--- users/views.py
+++ users/views.py
@@ -64,7 +64,7 @@ class current_user(viewsets.ReadOnlyModelViewSet):
     def retrieve(self, request, *args, **kwargs):
         return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED, data={"detail": "不允许此操作"})
     
-    @action(methods=['patch'], detail=False, url_path="change_password")
+    @action(methods=['post'], detail=False, url_path="change_password")
     def change_password(self, request, *args, **kwargs):
         try:
             user_obj =  User.objects.get(id = self.request.user.id)
